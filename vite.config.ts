1a774887eee867bece98c6099f03d75dc4c7918f
Sulaiman-F <s.alfawzan18@gmail.com>
2025-07-29 22:08:33 +0300

    Update preview configuration to enable host and allowed hosts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { defineConfig } from 'vite';
 export default defineConfig({
 	plugins: [tailwindcss(), sveltekit()],
 	preview: {
-		allowedHosts: ['all']
+		host: true,
+		allowedHosts: true
 	}
 });
